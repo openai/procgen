75b16d582403cdf931511d0bcaad8dd1f4b14326
Christopher Hesse <christopherhesse@users.noreply.github.com>
2020-01-10 14:58:04 -0800

    add dockerfile, fixes #23

---
+++ procgen/examples/random_agent.py
@@ -0,0 +1,14 @@
+"""
+Example random agent script to demonstrate that procgen works
+"""
+
+import gym
+env = gym.make('procgen:procgen-coinrun-v0')
+obs = env.reset()
+step = 0
+while True:
+    obs, rew, done, info = env.step(env.action_space.sample())
+    print(f"step {step} reward {rew} done {done}")
+    step += 1
+    if done:
+        break
