f4246f8ada660ea51d2cb55925893c5e438d2e2e
Christopher Hesse <christopherhesse@users.noreply.github.com>
2022-01-06 18:03:23 -0800

    .

--- procgen-build/setup.py
+++ procgen-build/setup.py
@@ -5,7 +5,6 @@ setup(
     packages=find_packages(),
     version="0.0.1",
     install_requires=[
-        "blobfile==0.8.0",
         # rather than rely on system cmake, install it here
         "cmake==3.15.3",
         # this is required by procgen/build.py
