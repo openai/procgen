7f81b674a9b8d40f0fe05add456aaa000dd11d9a
Christopher Hesse <christopherhesse@users.noreply.github.com>
2022-01-07 16:56:00 -0800

    .

--- procgen-build/setup.py
+++ procgen-build/setup.py
@@ -6,7 +6,7 @@ setup(
     version="0.0.1",
     install_requires=[
         # rather than rely on system cmake, install it here
-        "cmake==3.15.3",
+        "cmake==3.22.1",
         # this is required by procgen/build.py
         "gym3==0.3.0",
     ],
